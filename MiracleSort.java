eea4f6e08ed3cc79774d20c4d2576d0ae7e98ce9
Samyam <139134689+samyam81@users.noreply.github.com>
2024-01-29 16:23:05 +0545

    Add files via upload
    // Do nothing and hope that the array is already sorted

---
+++ MiracleSort.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MiracleSort {
+    public static void main(String[] args) {
+        int [] arr={1,2,4,5,3};
+        Miracle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void Miracle(int[] arr) {
+        // do nothing and hope the array is sorted...
+    }
+}
